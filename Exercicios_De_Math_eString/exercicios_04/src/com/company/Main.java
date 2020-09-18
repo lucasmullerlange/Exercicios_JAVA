aa57d0d52fd412cb6746b0b24c26191e5a1a813c
Lucas Lange <58046434+lucasmullerlange@users.noreply.github.com>
2020-09-18 20:35:21 -0300

    Update Main.java

--- Exercicios_De_Math_eString/exercicios_04/src/com/company/Main.java
+++ Exercicios_De_Math_eString/exercicios_04/src/com/company/Main.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class Main {
 
     public static void main(String[] args) {
-        String aux;
+        
         String frase = JOptionPane.showInputDialog(null, "Entre com uma frase qualquer:");
         StringBuffer sb = new StringBuffer(frase); JOptionPane.showMessageDialog(null, "frase invertida: " + sb.reverse());
 
